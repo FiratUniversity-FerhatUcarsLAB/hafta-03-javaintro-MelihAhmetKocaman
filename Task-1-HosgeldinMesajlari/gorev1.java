e849c282355c7d7b4c04fce123b38f792396afb3
250541088 <250541088@firat.edu.tr>
2025-10-21 00:16:38 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/gorev1.java
@@ -0,0 +1,25 @@
+
+public class gorev1 {
+    public static void main(String[] args) {
+
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+        System.out.println("Programlama Eğlencelidir");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("        J           A         V             V          A");
+        System.out.println("        J          A A         V           V          A A");
+        System.out.println("        J         A   A         V         V          A   A");
+        System.out.println("        J        A     A         V       V          A     A");
+        System.out.println("        J       A       A         V     V          A       A");
+        System.out.println("J       J      AAAAAAAAAAA         V   V          AAAAAAAAAAA");
+        System.out.println(" JJ   JJ      A           A         V V          A           A");
+        System.out.println("   JJJ       A             A         V          A             A");
+        System.out.println(" ");
+        System.out.println("-----------------------------------------------------------------");
+
+    }
+}
