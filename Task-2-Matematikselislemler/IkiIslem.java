63f486dc3194a9eabb658d49cfd33782335dbf9c
250541088 <250541088@firat.edu.tr>
2025-10-21 00:34:07 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,11 @@
+
+public class gorev2_2 {
+  public static void main(String[] args){
+
+    double pay = (9.5*4.5) - (2.5*3);
+        double payda = 45.5 - 3.5;
+        double sonuc = pay/payda;
+        System.out.println("((9.5*4.5) − (2.5*3))/(45.5 − 3.5)="+ sonuc);
+        System.out.printf("%.4f", sonuc);
+   }
+}
