49c51d22f5e9e96a4a5edcb214ba927ecd74021b
250541088 <250541088@firat.edu.tr>
2025-10-21 00:00:18 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,8 @@
+System.out.println("        J           A         V             V          A");
+System.out.println("        J          A A         V           V          A A");
+System.out.println("        J         A   A         V         V          A   A");
+System.out.println("        J        A     A         V       V          A     A");
+System.out.println("        J       A       A         V     V          A       A");
+System.out.println("J       J      AAAAAAAAAAA         V   V          AAAAAAAAAAA");
+System.out.println(" JJ   JJ      A           A         V V          A           A");
+System.out.println("   JJJ       A             A         V          A             A");
