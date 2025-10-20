de023e76150ba1b5b1d99a1839b7d93fe47d1d55
250541088 <250541088@firat.edu.tr>
2025-10-21 00:58:47 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,12 @@
+public class gorev3_1{
+  public static void main(String[] args){
+
+    double cap = 5.5;
+    double pi = 3.14159;
+    double daireninalan = pi * (cap * cap);
+    double dairenincevre = pi * 2 *cap;
+    System.out.println("Dairenin alanı =" + daireninalan);
+    System.out.println("Dairenin çevresi =" + dairenincevre);
+
+   }
+}
