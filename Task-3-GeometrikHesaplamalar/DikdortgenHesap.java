bcab46d26b449afb4778a210bb8461991f63eaa8
250541088 <250541088@firat.edu.tr>
2025-10-21 01:02:04 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+public class gorev3_2{
+  public static void main(String[] args){
+
+    double en = 4.5;
+    double boy = 7.9;
+    double dikdortgeninalan = en * boy;
+    double dikdortgenincevre = (en + boy)*2;
+    System.out.println("Dikdörgenin alanı= En x Boy= " + dikdortgeninalan);
+    System.out.println("Dikdörtgenin çevresi= 2xEn + 2xBoy= " + dikdortgenincevre);
+
+   }
+}
