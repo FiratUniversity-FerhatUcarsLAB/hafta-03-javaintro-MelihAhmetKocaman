7687b41543b5d120d5b5f006b7ab62bd4b73427d
250541088 <250541088@firat.edu.tr>
2025-10-21 00:08:57 +0300

    Update JavaDeseni.java

--- Task-1-HosgeldinMesajlari/JavaDeseni.java
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -1,8 +1,14 @@
-System.out.println("        J           A         V             V          A");
-System.out.println("        J          A A         V           V          A A");
-System.out.println("        J         A   A         V         V          A   A");
-System.out.println("        J        A     A         V       V          A     A");
-System.out.println("        J       A       A         V     V          A       A");
-System.out.println("J       J      AAAAAAAAAAA         V   V          AAAAAAAAAAA");
-System.out.println(" JJ   JJ      A           A         V V          A           A");
-System.out.println("   JJJ       A             A         V          A             A");
+
+pubic class gorev1_3
+public static void main(String[] args) {
+ 
+ System.out.println("        J           A         V             V          A");
+ System.out.println("        J          A A         V           V          A A");
+ System.out.println("        J         A   A         V         V          A   A");
+ System.out.println("        J        A     A         V       V          A     A");
+ System.out.println("        J       A       A         V     V          A       A");
+ System.out.println("J       J      AAAAAAAAAAA         V   V          AAAAAAAAAAA");
+ System.out.println(" JJ   JJ      A           A         V V          A           A");
+ System.out.println("   JJJ       A             A         V          A             A");
+
+}
