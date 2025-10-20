90d7817e1b8e74368e1f0ad87060f711fe55c0f1
250541088 <250541088@firat.edu.tr>
2025-10-21 00:46:03 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,15 @@
+public class gorev2_3{
+  public static void main(String[] args){
+
+int faktoriyel = 5*4*3*2*1;
+        int adim1 = 1*2;
+        int adim2 = 2*3;
+        int adim3 = 6*4;
+        System.out.println("5! faktöriyelini bulmak için;");
+        System.out.println("1.Adım: 1x2= " + adim1 );
+        System.out.println("2.Adım: 2x3= " + adim2 );
+        System.out.println("3.Adım: 6x4= " + adim3 );
+        System.out.println("4.Adım: 24x5= " + faktoriyel );
+        System.out.println("5!= " + faktoriyel );
+  }
+}
