0bc3f442aacaeaa00613a830b1d0c11e22181b89
250541088 <250541088@firat.edu.tr>
2025-10-23 22:55:18 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,26 @@
+public class gorev5_3{
+  public static void main(String[] args){
+
+        System.out.println("  İşlem  |  Sonuç ");
+        System.out.println("-------------------");
+        System.out.println("   2x1   |   2");
+        System.out.println("-------------------");
+        System.out.println("   2x2   |   4");
+        System.out.println("-------------------");
+        System.out.println("   2x3   |   6");
+        System.out.println("-------------------");
+        System.out.println("   2x4   |   8");
+        System.out.println("-------------------");
+        System.out.println("   2x5   |   10");
+        System.out.println("-------------------");
+        System.out.println("   2x6   |   12");
+        System.out.println("-------------------");
+        System.out.println("   2x7   |   14");
+        System.out.println("-------------------");
+        System.out.println("   2x8   |   16");
+        System.out.println("-------------------");
+        System.out.println("   2x9   |   18");
+        System.out.println("-------------------");
+        System.out.println("   2x10  |   20");
+    }
+}
