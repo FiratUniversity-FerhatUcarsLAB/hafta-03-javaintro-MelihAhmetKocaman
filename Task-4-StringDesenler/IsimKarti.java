ab422f4c527e531695c10aa2e9faf96cf1edaba7
250541088 <250541088@firat.edu.tr>
2025-10-23 22:43:53 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class gorev_4{
+    public static void main(String[] args){
+
+        String ad = "Melih Ahmet";
+        String soyad = "Kocaman";
+        int ogrno = 250541088;
+        String bolum = "Yazlımı Mühendisliği";
+        System.out.println("-------------------------------------");
+        System.out.println("| FIRAT ÜNİVERSİTESİ KİMLİK KARTI   |");
+        System.out.println("|  Ad: " + ad +"                  |");
+        System.out.println("|  Soyad: " + soyad +"                   |");
+        System.out.println("|  Öğrenci Numarası:" + ogrno +"       |");
+        System.out.println("|                                   |");
+        System.out.println("-------------------------------------");
+
+    }
+}
