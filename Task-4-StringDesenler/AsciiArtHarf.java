d1ea49f4751ecf208a54c8ba97f33499abf87e27
250541088 <250541088@firat.edu.tr>
2025-10-23 22:46:23 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class gorev_4{
+  public static void main(String[] args){
+    System.out.println("  __  __ \n" +
+                " |  \\/  |\n" +
+                " | \\  / |\n" +
+                " | |\\/| |\n" +
+                " | |  | |\n" +
+                " |_|  |_|\n" +
+                "         \n" +
+                "         ");
+  }
+}
+
+  
