a5de576dc55b3aab06fde71b21ae76db558417f0
250541088 <250541088@firat.edu.tr>
2025-10-23 22:48:35 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,14 @@
+public class gorev4_3{
+  public static void main(String[] args){
+
+  System.out.println("===============================");
+  System.out.println("|  ÜRÜN   |  MİKTAR |  FİYAT  |");
+  System.out.println("-------------------------------");
+  System.out.println("|  Elma   |   1 KG  |  15 TL  |");
+  System.out.println("-------------------------------");
+  System.out.println("|  Süt    | 1 LİTRE |  25 TL  |");
+  System.out.println("-------------------------------");
+  System.out.println("| Yumurta | 10 ADET |  30 TL  |");
+  System.out.println("-------------------------------");
+  System.out.println("| Ekmek   | 1 ADET  |  10 TL  |");
+  System.out.println("===============================");
