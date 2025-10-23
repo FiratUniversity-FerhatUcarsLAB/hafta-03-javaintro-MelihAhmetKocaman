391607a45902cb89ae070fb136dfcc1e5fa90929
250541088 <250541088@firat.edu.tr>
2025-10-23 22:49:26 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/gorev_4.java
@@ -0,0 +1,44 @@
+public class gorev_4{
+    public static void main(String[] args){
+
+        String ad = "Melih Ahmet";
+        String soyad = "Kocaman";
+        int ogrno = 250541088;
+        String bolum = "Yazlımı Mühendisliği";
+        System.out.println("-------------------------------------");
+        System.out.println("| FIRAT ÜNİVERSİTESİ KİMLİK KARTI   |");
+        System.out.println("|  Ad: " + ad +"                  |");
+        System.out.println("|  Soyad: " + soyad +"                   |");
+        System.out.println("|  Öğrenci Numarası:" + ogrno +"       |");
+        System.out.println("|                                   |");
+        System.out.println("-------------------------------------");
+        System.out.println(" ");
+
+        System.out.println("  __  __ \n" +
+                " |  \\/  |\n" +
+                " | \\  / |\n" +
+                " | |\\/| |\n" +
+                " | |  | |\n" +
+                " |_|  |_|\n" +
+                "         \n" +
+                "         ");
+        System.out.println(" ");
+
+        System.out.println("===============================");
+        System.out.println("|  ÜRÜN   |  MİKTAR |  FİYAT  |");
+        System.out.println("-------------------------------");
+        System.out.println("|  Elma   |   1 KG  |  15 TL  |");
+        System.out.println("-------------------------------");
+        System.out.println("|  Süt    | 1 LİTRE |  25 TL  |");
+        System.out.println("-------------------------------");
+        System.out.println("| Yumurta | 10 ADET |  30 TL  |");
+        System.out.println("-------------------------------");
+        System.out.println("| Ekmek   | 1 ADET  |  10 TL  |");
+        System.out.println("===============================");
+        System.out.println(" ");
+
+
+
+
+    }
+}
