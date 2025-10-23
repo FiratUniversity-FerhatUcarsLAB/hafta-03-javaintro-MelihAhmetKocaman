e8021e3f2d5d648b03da3d0d585dcba4eb455bcf
250541088 <250541088@firat.edu.tr>
2025-10-23 22:56:27 +0300

    Add files via upload

---
+++ Task-5-TabloDesenleri/gorev_5.java
@@ -0,0 +1,50 @@
+public class gorev_5 {
+    public static void main(String[] args) {
+
+        int a1 = 1;
+        int a2 = 2;
+        int a3 = 3;
+        int a4 = 4;
+        int a5 = 5;
+        System.out.println(" a    a^2    a^3");
+        System.out.println("-----------------");
+        System.out.println(" " + a1 + "    " + (a1*a1) + "      " + (a1*a1*a1));
+        System.out.println(" " + a2 + "    " + (a2*a2) + "      " + (a2*a2*a2));
+        System.out.println(" " + a3 + "    " + (a3*a3) + "      " + (a3*a3*a3));
+        System.out.println(" " + a4 + "    " + (a4*a4) + "     " + (a4*a4*a4));
+        System.out.println(" " + a5 + "    " + (a5*a5) + "     " + (a5*a5*a5));
+        System.out.println(" ");
+
+        int km = 1609;
+        System.out.println(" Mil --> Kilometre");
+        System.out.println("--------------------");
+        System.out.println("  1  --> " + (1*km));
+        System.out.println("  5  --> " + (5*km));
+        System.out.println(" 10  --> " + (10*km));
+        System.out.println(" 20  --> " + (20*km));
+        System.out.println(" 50  --> " + (50*km));
+        System.out.println(" ");
+
+        System.out.println("  İşlem  |  Sonuç ");
+        System.out.println("-------------------");
+        System.out.println("   2x1   |   2");
+        System.out.println("-------------------");
+        System.out.println("   2x2   |   4");
+        System.out.println("-------------------");
+        System.out.println("   2x3   |   6");
+        System.out.println("-------------------");
+        System.out.println("   2x4   |   8");
+        System.out.println("-------------------");
+        System.out.println("   2x5   |   10");
+        System.out.println("-------------------");
+        System.out.println("   2x6   |   12");
+        System.out.println("-------------------");
+        System.out.println("   2x7   |   14");
+        System.out.println("-------------------");
+        System.out.println("   2x8   |   16");
+        System.out.println("-------------------");
+        System.out.println("   2x9   |   18");
+        System.out.println("-------------------");
+        System.out.println("   2x10  |   20");
+    }
+}
