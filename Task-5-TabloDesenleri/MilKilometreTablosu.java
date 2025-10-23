27704d191798a461f3f55d7595e6eca7c188b3a5
250541088 <250541088@firat.edu.tr>
2025-10-23 22:53:56 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,14 @@
+public class gorev_5 {
+    public static void main(String[] args) {
+
+  int km = 1609;
+  System.out.println(" Mil --> Kilometre");
+  System.out.println("--------------------");
+  System.out.println("  1  --> " + (1*km));
+  System.out.println("  5  --> " + (5*km));
+  System.out.println(" 10  --> " + (10*km));
+  System.out.println(" 20  --> " + (20*km));
+  System.out.println(" 50  --> " + (50*km));
+
+    }
+}
